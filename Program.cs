d78720e2c55d3d597cd406c21bb2cea652836638
Tasha Lane <designtmg77@gmail.com>
2020-08-27 15:53:00 -0500

    multi-sting interpolation

--- Program.cs
+++ Program.cs
@@ -10,23 +10,44 @@ namespace Friends
 
             string aFriend = "Steve";
             Console.WriteLine(aFriend);
+            Console.WriteLine("Hello " + aFriend);
 
             aFriend = "Brenda";
             Console.WriteLine(aFriend);
+            Console.WriteLine("Hello " + aFriend);
 
             aFriend = "Andy";
             Console.WriteLine(aFriend);
+            Console.WriteLine("Hello " + aFriend);
 
             aFriend = "Mo";
             Console.WriteLine(aFriend);
+            Console.WriteLine("Hello " + aFriend);
 
             aFriend = "Rose";
             Console.WriteLine(aFriend);
+            Console.WriteLine("Hello " + aFriend);
 
             aFriend = "Sage";
             Console.WriteLine(aFriend);
-
             Console.WriteLine("Hello " + aFriend);
+
+            //String interpolation
+            aFriend = "Gavin";
+            Console.WriteLine(aFriend);
+            Console.WriteLine($"Hey {aFriend}");
+
+            string firstFriend = "Gavin";
+            string secondFriend = "Amanda";
+            string thirdFriend = "Kelly";
+            string fourthFriend = "Faith";
+            string fifthFriend = "Anthony";
+            string sixthFriend = "James";
+            string seventhFriend = "John";
+            string eighthFriend = "Tyler";
+            string ninthFriend = "Brandon";
+            Console.WriteLine($"My friends are {firstFriend},{secondFriend},{thirdFriend},{fourthFriend},{fifthFriend},{sixthFriend},{seventhFriend},{eighthFriend} and {ninthFriend}");
+
         }
     }
 }
